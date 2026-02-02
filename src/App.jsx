2af2fb9d9607ecc7f1967e53bbf2bf11acd5652b
Sayan Mallick <malsayan6@gmail.com>
2026-02-02 22:10:54 +0530

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import NavBar from './components/navbar'
+import Hero from './components/hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
  <NavBar />
+ <Hero />
     </>
   )
 }
