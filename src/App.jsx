d88754dcd0fd1dd701c47f4d3a9a6844b0a8ce35
Sayan Mallick <malsayan6@gmail.com>
2026-02-03 10:11:36 +0530

    3d macbook

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import NavBar from './components/navbar'
 import Hero from './components/hero'
 import ProductViewer from './components/productView'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/productView.jsx
+++ src/components/productView.jsx
@@ -2,10 +2,10 @@ import useMacbookStore from "../store";
 import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
-import MacbookModel14 from "./models/Macbook-14.jsx";
+import MacbookModel14 from "../models/Macbook-14.jsx";
 
-import StudioLights from "./three/StudioLights.jsx";
-import ModelSwitcher from './three/ModelSwitcher.jsx'
+import StudioLights from "../three/StudioLights.jsx";
+import ModelSwitcher from '../three/ModelSwitcher.jsx'
 import {useMediaQuery} from "react-responsive";
 
 const ProductViewer = () => {
