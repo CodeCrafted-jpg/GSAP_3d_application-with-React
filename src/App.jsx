a4bba0ea8b67ee43c250848c0a5b6cbcc4ddbd2b
Sayan Mallick <malsayan6@gmail.com>
2026-02-03 10:34:54 +0530

    componets

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ import Hero from './components/hero'
 import ProductViewer from './components/productView'
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
+import Showcase from './components/showcase'
+import Performance from './components/Performece'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+ <main>
  <NavBar />
  <Hero />
  <ProductViewer />
-    </>
+ <Showcase />
+ <Performance />
+ <Features />
+ <Highlights/>
+ <Footer />
+</main> 
   )
 }
 
